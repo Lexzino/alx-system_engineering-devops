858ab7850112a0a567cabada9880af6977282e2e
Lexzino <alexudeze001@gmal.com>
2024-03-22 19:30:27 -0700

    0-create_a_file.pp

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,4 +1,11 @@
-# creates a file in the /tmp
+#  Create a file in '\tmp' directory with the requirements below
+#  Filename: school
+#  File content: I love Puppet
+#  File permissions: 0744
+#  File owner: www-data
+#  File group: www-data
+#  File location: /tmp/school
+
 file{ '0-create_a_file':
   ensure  => 'file',
   path    => '/tmp/school',
