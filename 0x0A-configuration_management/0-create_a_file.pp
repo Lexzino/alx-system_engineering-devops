e96d5bf56bfb69dced71bb3328758b73afdba13b
Lexzino <alexudeze001@gmal.com>
2024-03-26 15:36:55 -0700

    task complted

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,8 +1,9 @@
-# creates a file in the /tmp
+# Creates a file
 file { '/tmp/school':
   ensure  => file,
-  content => 'I love Puppet',
+  path    => '/tmp/school',
   mode    => '0744',
   owner   => 'www-data',
   group   => 'www-data',
+  content => 'I love Puppet'
 }
--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,5 @@
-# installs the package puppet-lint
+# Install flask
 package { 'flask':
   ensure   => '2.1.0',
-  provider => 'pip3',
+  provider => 'pip3'
 }
