fe3e0962ea945d4ff2cf7da8db0e7c02b4eca913
lexzino <alexudeze001@gmail.com>
2024-04-22 05:19:53 -0700

    task completed

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""Gather data from an API"""
+
+import requests
+import sys
+
+
+def getInformation(employeeid):
+    """To returns information based on ID"""
+    url = "https://jsonplaceholder.typicode.com/"
+    endpoint = url + 'users/{}'.format(employeeid)
+    employee = requests.get(endpoint).json()
+    taskendpoint = url + 'TODOs?userId={}'.format(employee.get('id'))
+    tasks = requests.get(taskendpoint).json()
+    data = {"employee": employee, "tasks": tasks}
+    totalTasks = len(data['tasks'])
+    tasks = [task for task in data['tasks'] if task['completed']]
+    completedTasks = len(tasks)
+    print('Employee {} is done with tasks({}/{}):'
+          .format(data['employee']['name'], completedTasks, totalTasks))
+    for task in tasks:
+        print('\t {}'.format(task['title']))
+
+
+if __name__ == '__main__':
+    getInformation(sys.argv[1])
