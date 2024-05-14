e2d2b7bf8219b46d887addfae9def51d3e05abda
Lexzino <alexudeze001@gmail.com>
2024-05-15 00:17:41 +0100

    task completed

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,8 @@
+fix nginx to accdpt and handle more request.
+
+exec { 'set_ulimit_to 5000':
+  command => '/bin/sed -i "s/ULIMIT.*/ULIMIT=\"-n 5000\"/" /etc/default/nginx'
+
+} -> exec { 'restart nginx':
+  command => '/usr/sbin/service nginx restart',
+}
